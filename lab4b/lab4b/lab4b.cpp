7e3ef8cc2f5884c29d17cb83d2efaaba3d099d97
kirkin17 <kirkin17@users.noreply.github.com>
2019-12-22 20:18:47 +0300

    v1

---
+++ lab4b/lab4b/lab4b.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[5], b[5];
+	int tmpa,tmpb;
+	cout << "Ввод матрицы А: \n";
+	for (int i; i <= 5; i++)
+	{
+		cin >> a[i];
+	}
+	cout << "Ввод матрицы B: \n";
+	for (int i; i <= 4; i++)
+	{
+		cin >> b[i];
+	}
+	for (int i; i <= 4; i++)
+	{
+		if (a[i] < 0) 
+		{ 
+			tmpb = a[i];
+			if (b[i] == 0) { b[i] = tmpb; }
+			else 
+			{
+				for (int k; k <= 4; k++)
+				{
+					if (b[k] == 0) { b[k] = tmpb; };
+					if (b[k] > 0) 
+					{ 
+						tmpa = b[k];
+						if (a[k] == 0) { a[k] = tmpa; }
+						else 
+						{
+							for (int j; j <= 4; j++)
+							{
+								if (a[i]==0) {a[i]=tmpa }
+								if (a[i]) 
+							}
+						}
+					}
+				}
+			}
+		};
+		if (b[i] >= 0) { tmpa = b[i]; };
+	}
+}
